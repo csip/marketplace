d45a764a3ec22eb4f3c1286ec5f047750ee867cd
Baishi Wu <baishi@gmail.com>
2013-07-16 17:32:21 +0700

    Updated Footer Styles

--- html/wp-content/themes/twentytwelve/footer.php
+++ html/wp-content/themes/twentytwelve/footer.php
@@ -14,9 +14,21 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
-			<p>CENTRE FOR SOCIAL INITIATIVES PROMOTION (CSIP)</p>
+			<a href="/"><img class="footer-logo" src="/wp-content/uploads/2013/07/csip-logo.png"></img></a>
+		</div>
+		<div class="footer-links">
+			<ul>
+				<li style="font-weight:bold; color:#686868">Information</li>				
+				<li><a style="text-decoration:none" href="/">About</a></li>
+				<li><a style="text-decoration:none" href="/">Blog</a></li>
+				<li><a style="text-decoration:none" href="/">Contact</a></li>				
+			</ul>
+		</div>
+		<div class="footer-address">
+			<p style="font-weight:bold"><a style="text-decoration:none" href="http://csip.vn/">Center for Social Initiatives Promotion (CSIP)</a></p>
 			<p>Room 14*06B, 14*th Floor, B Tower, Ha Thanh Plaza, 102 Thai Thinh Str., Dong Da Dist., Hanoi</p>
-			<p>Tel: +84 435378746 Fax: +84 435378992 / Email: csipvn@gmail.com</p>
+			<p>Tel: +84 435378746 Fax: +84 435378992 / Email: <a style="text-decoration:none" href="mailto:csipvn@gmail.com">csipvn@gmail.com</a></p>
+			<p>Copyright &copy; CSIP 2009-2013</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
